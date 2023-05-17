0dc4c252a89287e030cf41bfc40b796916afe9e7
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-05-17 11:57:44 +0200

    add Introduction slide

--- src/chapters/Introduction.tsx
+++ src/chapters/Introduction.tsx
@@ -1,5 +1,5 @@
 import { H1, H2, Slide } from "@gregcello/revealjs-react"
-import { Grid } from "@mui/material"
+import { Box, Grid, Paper, Typography } from "@mui/material"
 import MainSlide from "../components/MainSlide"
 import Outline from "../slides/Outline"
 import BasicVariogram from "../slides/BasicVariogram"
@@ -24,7 +24,22 @@ const Introduction: React.FC = () => {
 
         <Outline withFragments />
 
-        <MainSlide autoAnimate title="Motivation" id="start-motivation">
+        <MainSlide title="Why?" id="start-motivation">
+            <Paper elevation={3} sx={{padding: '0.3rem'}}>
+                <Box src="img/kidd_fig1.jpg" component="img" sx={{maxHeight: 400}} />
+                <Typography variant="caption" component="div" sx={{textAlign: 'left', mt: 1}}>
+                    Distance to the next professionally operated rain gauge, global coverage. Figure taken from <a href="https://journals.ametsoc.org/view/journals/bams/98/1/bams-d-14-00283.1.xml#:~:text=View%20Full%20Size-,Fig.%201.,-Map%20showing%20the" target="_blank" rel="noreferrer">Kidd et al. (2017)</a>.
+                </Typography>
+            </Paper>
+            <aside className="notes">
+                <ul>
+                    <li>Germany (2003): 601.6m² of rain gauges</li>
+                    <li>if representative for 1km around, still only 1% of earth surface</li>
+                </ul>
+            </aside>
+        </MainSlide>
+
+        <MainSlide autoAnimate title="Motivation">
             <BasicVariogram />
 
         </MainSlide>
