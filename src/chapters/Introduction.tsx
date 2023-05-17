1c4f763b24a04b49e7357fd7820c929547d0fe17
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-05-17 14:30:42 +0200

    create a Starzel example in Introduction

--- src/chapters/Introduction.tsx
+++ src/chapters/Introduction.tsx
@@ -26,7 +26,7 @@ const Introduction: React.FC = () => {
 
         <MainSlide title="Why?" id="start-motivation">
             <Paper elevation={3} sx={{padding: '0.3rem'}}>
-                <Box src="img/kidd_fig1.jpg" component="img" sx={{maxHeight: 400}} />
+                <Box component="img" src="img/kidd_fig1.jpg" sx={{maxHeight: 400}} />
                 <Typography variant="caption" component="div" sx={{textAlign: 'left', mt: 1}}>
                     Distance to the next professionally operated rain gauge, global coverage. Figure taken from <a href="https://journals.ametsoc.org/view/journals/bams/98/1/bams-d-14-00283.1.xml#:~:text=View%20Full%20Size-,Fig.%201.,-Map%20showing%20the" target="_blank" rel="noreferrer">Kidd et al. (2017)</a>.
                 </Typography>
@@ -37,6 +37,20 @@ const Introduction: React.FC = () => {
                     <li>if representative for 1km around, still only 1% of earth surface</li>
                 </ul>
             </aside>
+        </MainSlide>
+
+        <MainSlide title="Why?">
+            <Paper elevation={3} sx={{padding: '0.3rem'}}>
+                <Grid container spacing={1}>
+                    <Grid item xs={12} md={6}>
+                        <Box component="img" src="img/starzel_radar.jpg" sx={{maxHeight: 400}} />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Box component="img" src="img/starzel_model.jpg" sx={{maxHeight: 400}} className="fragment custom blur transparent" />
+                    </Grid>
+                </Grid>
+                
+            </Paper>
         </MainSlide>
 
         <MainSlide autoAnimate title="Motivation">
