f776a4baab6471e723f1f7e66a01e38d3d3bec46
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-05-17 11:58:02 +0200

    change MasterSlider

--- src/components/MainSlide.tsx
+++ src/components/MainSlide.tsx
@@ -1,5 +1,5 @@
 import { Slide, SlideProps, H2 } from "@gregcello/revealjs-react"
-import { Grid } from "@mui/material"
+import { Grid, Typography } from "@mui/material"
 import React from "react"
 
 interface MainSlideProps extends SlideProps {
@@ -9,17 +9,19 @@ interface MainSlideProps extends SlideProps {
 const MainSlide: React.FC<React.PropsWithChildren<MainSlideProps>> = ({ children, title, ...slideProps }) => {
     return (
         <Slide {...slideProps}>
-            <Grid container spacing={0} className="justify-col" sx={{minHeight: '100vh', maxHeight: '100vh', p: 3}}>
-                <Grid item xs={12} className="justify-row">
+            <Grid container spacing={0} className="justify-col" sx={{minHeight: '100vh', maxHeight: '100vh', p: 0}}>
+                <Grid item xs={12} className="justify-row" sx={{height: '65px'}}>
                     <img src="img/logo_kit.png" alt="KIT logo" style={{maxHeight: '65px'}} />
                     { title ? <H2>{ title }</H2> : null}
                     <span />
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={12} sx={{height: 'calc(100% - 125px)'}}>
                     { children }
                 </Grid>
-                <Grid item xs={12}>
-                    Footer line without content
+                <Grid item xs={12} sx={{height: '40px'}}>
+                    <Typography variant="caption" component="div" sx={{textAlign: 'center'}}>
+                        Mälicke - PhD Defense - geostatistical Software - (17.07.2023)
+                    </Typography>
                 </Grid>
             </Grid>
         </Slide>
