2c284baad57b19cc261652408d34839cee5307f9
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-05-25 15:03:35 +0200

    implement a better layout

--- src/components/MainSlide.tsx
+++ src/components/MainSlide.tsx
@@ -1,5 +1,5 @@
 import { Slide, SlideProps, H2 } from "@gregcello/revealjs-react"
-import { Grid, Typography } from "@mui/material"
+import { Box, Grid, Typography } from "@mui/material"
 import React from "react"
 
 interface MainSlideProps extends SlideProps {
@@ -9,21 +9,21 @@ interface MainSlideProps extends SlideProps {
 const MainSlide: React.FC<React.PropsWithChildren<MainSlideProps>> = ({ children, title, ...slideProps }) => {
     return (
         <Slide {...slideProps}>
-            <Grid container spacing={0} className="justify-col" sx={{minHeight: '100vh', maxHeight: '100vh', p: 0}}>
-                <Grid item xs={12} className="justify-row" sx={{height: '65px'}}>
+            
+                <Box className="justify-row" sx={{height: '65px'}}>
                     <img src="img/logo_kit.png" alt="KIT logo" style={{maxHeight: '65px'}} />
                     { title ? <H2>{ title }</H2> : null}
                     <span />
-                </Grid>
-                <Grid item xs={12} sx={{height: 'calc(100% - 125px)'}}>
+                </Box>
+                <Box className="r-stretch" sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', m: 1}}>
                     { children }
-                </Grid>
-                <Grid item xs={12} sx={{height: '40px'}}>
+                </Box>
+                <Box sx={{height: '40px'}}>
                     <Typography variant="caption" component="div" sx={{textAlign: 'center'}}>
                         Mälicke - PhD Defense - geostatistical Software - (17.07.2023)
                     </Typography>
-                </Grid>
-            </Grid>
+                </Box>
+
         </Slide>
     )
 }
