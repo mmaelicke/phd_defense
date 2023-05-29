0eee3d2a6c767e06dfa22114dda6ea14ea967785
Mirko Mälicke <mirko@hydrocode.de>
2023-05-29 10:29:49 +0200

    add to chapter

--- src/chapters/Uncertainty.tsx
+++ src/chapters/Uncertainty.tsx
@@ -1,12 +1,19 @@
 import { Paper } from "@mui/material"
 import MainSlide from "../components/MainSlide"
 import BasicUncertainty from "../slides/BasicUncertainty"
+import BasicUncertainModels from "../slides/BasicUncertainModels"
 
 const Uncertainty: React.FC = () => {
     return (<>
         <MainSlide title="Uncertainty" id="start-uncertainty">
             <Paper elevation={3} sx={{p: 2}}>
                 <BasicUncertainty />
+            </Paper>
+        </MainSlide>
+
+        <MainSlide title="Uncertain Models">
+            <Paper elevation={3} sx={{p: 2}}>
+                <BasicUncertainModels />
             </Paper>
         </MainSlide>
     </>)
