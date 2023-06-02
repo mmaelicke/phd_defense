c8b3bfcba1b24edda879d88413300a85f37b694d
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-06-02 12:40:05 +0200

    change slide behaviour

--- src/slides/ParallelMetrics.tsx
+++ src/slides/ParallelMetrics.tsx
@@ -5,7 +5,7 @@ import MODELS_DATA from "./data/theoretical_models.json";
 import PARCAT_DATA from "./data/metric_parcat.json";
 import Plot from "react-plotly.js";
 import { Data, Layout, ScatterData } from "plotly.js";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 
 
@@ -13,15 +13,18 @@ const ParallelMetrics = () => {
     // state to control the tab selection
     const [tab, setTab] = useState<'models' | 'metrics'>('models');
 
-    return (<>
+    // add a ref to the container
+    const containerRef = useRef<HTMLDivElement>()
+
+    return (<Box sx={{width: '100%'}} component="div" ref={containerRef}>
         <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
             <Tabs value={tab} onChange={(_, val) => setTab(val)}>
-                <Tab value="models" label="Theoretical models" />
-                <Tab value="metrics" label="Performance metrics" />
+                <Tab value="models" label="Models" />
+                <Tab value="metrics" label="Metrics" />
             </Tabs>
         </Box>
         { tab === 'models' ? (
-            <Slide in direction="right">
+            <Slide in direction="right" container={containerRef.current}>
                 <Box>
                 <Plot
                     data={MODELS_DATA.data as unknown as ScatterData[]}
@@ -32,7 +35,7 @@ const ParallelMetrics = () => {
             </Slide>
         ) : null }
         { tab === 'metrics' ? (
-            <Slide in direction="left">
+            <Slide in direction="left" container={containerRef.current}>
                 <Box>
                     <Plot
                         data={PARCAT_DATA.data as unknown as Data[]}
@@ -42,7 +45,7 @@ const ParallelMetrics = () => {
                 </Box>
             </Slide>
         ) : null }
-    </>)
+    </Box>)
 }
 
 export default ParallelMetrics
