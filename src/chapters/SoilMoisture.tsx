e7fd390d75dd22393dec08a99b0eda88ffbafb9f
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-06-06 12:36:10 +0200

    added soil moisture example

--- src/chapters/SoilMoisture.tsx
+++ src/chapters/SoilMoisture.tsx
@@ -1,13 +1,19 @@
+import { Box, Paper } from "@mui/material"
+
 import MainSlide from "../components/MainSlide"
 import Outline from "../slides/Outline"
 
 const SoilMoisture: React.FC = () => {
     return (<>
 
-        <Outline highlight="moisture" id="start-soil-moisture" />
+        <Outline highlight="moisture" />
 
-        <MainSlide title="Soil Moisture">
-            <h1>Soil Moisture</h1>
+        <MainSlide title="Soil Moisture" id="start-soil-moisture" autoAnimate>
+            <Paper elevation={3} className="r-stack">
+                <Box component="img" src="img/moisture_cluster_step1.png" sx={{maxHeight: '450px'}}  />
+                <Box component="img" src="img/moisture_cluster_step2.gif" sx={{maxHeight: '450px'}} className="fragment" />
+                <Box component="img" src="img/moisture_cluster_step3.png" sx={{maxHeight: '450px'}} className="fragment" />
+            </Paper>
         </MainSlide>
     </>)
 }
