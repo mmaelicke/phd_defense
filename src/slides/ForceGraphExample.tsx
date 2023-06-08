d5d80e8c867e6ee9eb14765dc6b7f4019457eec2
Mirko Mälicke <mirko@hydrocode.de>
2023-06-08 15:27:23 +0200

    first graph example

--- src/slides/ForceGraphExample.tsx
+++ src/slides/ForceGraphExample.tsx
@@ -1,17 +1,22 @@
-import { Grid, Paper } from '@mui/material'
+import { Button, Grid, Paper, Slider, Stack } from '@mui/material'
 import { useEffect, useRef, useState } from 'react'
-import { ForceGraph3D } from 'react-force-graph'
+import { ForceGraph3D, ForceGraph2D } from 'react-force-graph'
 
 import './ForceGraphExample.css'
+import cloneDeep from 'lodash.clonedeep'
 
 // generate some sample data
 const SAMPLE = {
-    nodes: [{id: "1"}, {id: "2"}, {id: "3"}, {id: "4"}],
+    nodes: [{id: "1", size: 3, name: 'Observation 1'}, {id: "2", size: 3, name: 'Observation 2'}, {id: "3", size: 3, name: 'Obersavtion 3'}],
     links: [
-        {source: "1", target: "2", distance: 10}, {source: "2", target: "3", distance: 30}, {source: "3", target: "1", distance: 20},
-        {source: "1", target: "4", distance: 100}, {source: "3", target: "4", distance: 50}
+        {source: "1", target: "2", distance: 10, color: 'green', label: '10'}, 
+        {source: "2", target: "3", distance: 30, color: 'red', label: '30'},
     ]
 }
+
+// set the number of nodes for the large graph
+const N = 25
+const K = 100
 
 interface Node {
     id: string
@@ -25,9 +30,14 @@ interface Link {
     strength?: number
     color?: string
     label?: string
+    name?: string
 }
 
 const ForceGraphExample = () => {
+    // state for controlling the current step
+    const [withTriangle, setWithTriangle] = useState<boolean>(false)
+    const [withLargeGraph, setWithLargeGraph] = useState<boolean>(false)
+
     // get a reference to the Paper
     const containerRef = useRef<HTMLDivElement>(null)
 
@@ -35,43 +45,122 @@ const ForceGraphExample = () => {
     const graphRef = useRef<any>(null)
     
     // state for graph data
-    const [nodes, setNodes] = useState<Node[]>(SAMPLE.nodes)
-    const [links, setLinks] = useState<Link[]>(SAMPLE.links)
+    const [graphData, setGraphData] = useState<{nodes: Node[], links: Link[]}>(SAMPLE)
 
 
     // adjust the distance and stregth of the links, when links change
     useEffect(() => {
         if (graphRef.current) {
-            //graphRef.current.d3Force('charge', () => 0.1);
+            //graphRef.current.d3Force('link', null);
             (graphRef.current).d3Force('link')
                 .distance((link: Link) => link.distance || 5)
                 //.strength((link: Link) => link.strength || 0.1)
                 //.iterations(() => 100)
         }
-    }, [links])
+    }, [])
+
+    // handle slider change
+    const onLinkChange = (index: number, value: number) => {
+        if (index >= graphData.links.length) return
+
+        // copy the graph data
+        const newGraphData = cloneDeep(graphData)
+        newGraphData.links[index].distance = value
+        newGraphData.links[index].label = `${value}`
+
+        // update the graph data
+        setGraphData(newGraphData)
+    }
+
+    // add or remove a new link to the graph if withTriangle changes
+    useEffect(() => {
+        // copy the graph data
+        const newGraphData = cloneDeep(graphData)
+
+        // add or remove the link
+        if (withTriangle) {
+            newGraphData.links.push({source: "1", target: "3", distance: 10, color: 'blue', label: '10'})
+        } else {
+            newGraphData.links = newGraphData.links.filter(l => l.color !== 'blue')
+        }
+
+        // update the graph data
+        setGraphData(newGraphData)
+    }, [withTriangle])
+
+    // if withLargeGraphe changes, add 15 random nodes and 30 random links, or remove them
+    useEffect(() => {
+        // copy the graph data
+        const newGraphData = cloneDeep(graphData)
+
+        // add or remove the nodes
+        if (withLargeGraph) {
+            for (let i = 4; i <= N; i++) {
+                newGraphData.nodes.push({id: `${i}`, size: 3})
+            }
+            for (let i = 0; i <= K; i++) {
+                // with a chance of 30%, connect one of the first 3 nodes to a random node
+                let ii: number
+                if (Math.random() < 0.3) {
+                    ii = Math.floor(Math.random() * 2.99) + 1
+                } else {
+                    ii = Math.floor(Math.random() * (N - 3.01)) + 4
+                }
+                    
+                // get a random connection
+                let ij = Math.floor(Math.random() * (N - 3.01)) + 4
+                while (ii === ij) {
+                    ij = Math.floor(Math.random() * (N - 3.01)) + 4
+                }
+                newGraphData.links.push({source: `${ii}`, target: `${ij}`, distance: Math.floor(Math.random() * 50) + 1, color: 'gray', name: 'random'})
+            }
+        } else {
+            newGraphData.nodes = newGraphData.nodes.filter(n => parseInt(n.id) < 4)
+            newGraphData.links = newGraphData.links.filter(l => l.name! !== 'random')
+        }
+
+        // update the graph data
+        setGraphData(newGraphData)
+    }, [withLargeGraph])
 
     // render
     return (
         <Paper elevation={3} sx={{p: 1}}>
             <Grid container>
                 
-                <Grid item xs={4}>
-                    <h3>Nodes</h3>
+                <Grid item xs={4} justifyContent="space-around" direction="column" display="flex">
+                    <Stack sx={{p: 3}}>
+                        <Slider min={1} max={100} defaultValue={10} sx={{color: 'success.main'}} valueLabelDisplay="auto" onChange={(e, value) => onLinkChange(0, value as number)} />
+                        <Slider min={1} max={100} defaultValue={30} sx={{color: 'error.main'}} valueLabelDisplay="auto" onChange={(e, value) => onLinkChange(1, value as number)} />
+                        { withTriangle ? (
+                            <Slider min={1} max={100} defaultValue={10} sx={{color: 'primary.main'}} valueLabelDisplay="auto" onChange={(e, value) => onLinkChange(2, value as number)} />
+                        ) : null }
+                    </Stack>
+                    <Stack sx={{p: 3}}>
+                        <Button variant="outlined" color={withTriangle ? 'error' : 'warning'} onClick={() => setWithTriangle(!withTriangle)}>
+                            {withTriangle ? 'Remove blue' : 'Triangle'}
+                        </Button>
+                        <Button variant="outlined" color={withLargeGraph ? 'error' : 'success'} onClick={() => setWithLargeGraph(!withLargeGraph)}>
+                            {withLargeGraph ? 'Small Graph' : 'Large Graph' }
+                        </Button>
+                    </Stack>
                 </Grid>
                 
-                <Grid item xs={8} component="div" ref={containerRef}>
+                <Grid item xs={8} component="div" ref={containerRef} sx={{p: 0, borderRadius: '5px'}}>
                     <ForceGraph3D
                         ref={graphRef} 
-                        graphData={{nodes, links}} 
+                        graphData={graphData} 
                         nodeVal="value"
                         height={450} 
                         width={containerRef.current ? containerRef.current.clientWidth : 400}
-                        backgroundColor="white" 
-                        linkColor={() => 'rgb(0,0,0)'} 
-                        nodeColor={() => '#2EA28A'} 
+                        backgroundColor='rgba(0,0,0,0.0)'
+                        linkColor={l => l.color ? l.color : 'rgb(0,0,0)'} 
+                        linkOpacity={0.8}
+                        nodeColor={() => '#2EA28A'}
+                        linkLabel={'label'}
                         linkWidth={2}
                         forceEngine="d3"
-                        
+                        enableNodeDrag
                     />
                 </Grid>
 
