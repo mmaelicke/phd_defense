ac23b026b46747bfe8174d89693f1d4cec430913
Mirko Mälicke <mirko@hydrocode.de>
2023-06-09 08:24:34 +0200

    add GraphExample Tabs

---
+++ src/slides/GraphExamples.tsx
@@ -0,0 +1,20 @@
+import { Box, Tab, Tabs } from "@mui/material"
+import { useState } from "react"
+
+const GraphExamples: React.FC = () => {
+    // state to handle the current tab
+    const [tab, setTab] = useState<number>(1)
+
+    return (<>
+        <Box sx={{borderBottom: '1px', borderColor: 'divider'}}>
+            <Tabs value={tab} onChange={(_, value) => setTab(value as number)}>
+                <Tab label="Random distance matrix" value={1} />
+                <Tab label="Meshgrid - high variance" value={2} />
+                <Tab label="Meshgrid - no variance" value={3} />
+            </Tabs>
+        </Box>
+        <Box component="img" src={`img/benchmark_graph_${tab}.gif`} alt="Animation of graph" />
+        </>)
+}
+
+export default GraphExamples
