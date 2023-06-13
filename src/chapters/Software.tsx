dbdffe1e17763d3cb27422fbca2fcb9da949e7ae
Mirko Mälicke <mirko@hydrocode.de>
2023-06-13 08:13:49 +0200

    add softwareFlowchart software chapter

--- src/chapters/Software.tsx
+++ src/chapters/Software.tsx
@@ -1,6 +1,8 @@
 import { Paper } from "@mui/material"
+
 import MainSlide from "../components/MainSlide"
 import Outline from "../slides/Outline"
+import SoftwareFlowchart from "../slides/SoftwareFlowchart"
 
 
 const Software: React.FC = () => {
@@ -11,6 +13,10 @@ const Software: React.FC = () => {
             <Paper elevation={3} sx={{height: '100%'}}>
             <iframe data-src="https://skg.geostat.hydrocode.de" style={{width: '100%', height: '100%'}} frameBorder="0" title="Software example"/>
             </Paper>
+        </MainSlide>
+
+        <MainSlide title="Software Overview">
+            <SoftwareFlowchart />
         </MainSlide>
     </>)
 }
