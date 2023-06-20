d489eb5e80029f613e4311505a6cf374cd07ec0f
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-06-20 13:24:24 +0200

    add an extra LI component

---
+++ src/components/LI.tsx
@@ -0,0 +1,14 @@
+import { Typography, TypographyProps } from "@mui/material"
+
+type TypographyVariant = TypographyProps["variant"]
+
+interface LIProps extends React.PropsWithChildren {
+    variant?: TypographyVariant
+    className?: string
+}
+
+const LI: React.FC<LIProps> = ({ children, variant, className }) => (
+    <Typography variant={variant || "h6"} component="div" className={className || ""}>{children}</Typography>
+)
+
+export default LI
