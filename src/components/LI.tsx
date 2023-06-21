7561460998f606021f711b3b9ee72080bd4795f8
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-06-21 13:27:22 +0200

    expose sx prop for LI

--- src/components/LI.tsx
+++ src/components/LI.tsx
@@ -6,11 +6,19 @@ type SX = TypographyProps["sx"]
 interface LIProps extends React.PropsWithChildren {
     variant?: TypographyVariant
     className?: string
+    sx?: SX
     
 }
 
-const LI: React.FC<LIProps> = ({ children, variant, className }) => (
-    <Typography variant={variant || "h6"} component="div" className={className || ""}>{children}</Typography>
+const LI: React.FC<LIProps> = ({ children, variant, className, sx }) => (
+    <Typography 
+        variant={variant || "h6"} 
+        component="div" 
+        className={className || ""}
+        sx={{textAlign: "left", ...sx}}
+    >
+            {children}
+    </Typography>
 )
 
 export default LI
