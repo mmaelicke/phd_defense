b346b4f258e04e06af09f1e2f697ce2dc0275617
Mirko Mälicke <mirko@hydrocode.de>
2023-06-24 07:00:19 +0200

    show different benchmark graph fields

--- src/slides/GraphExamples.tsx
+++ src/slides/GraphExamples.tsx
@@ -1,4 +1,4 @@
-import { Box, Tab, Tabs } from "@mui/material"
+import { Box, Grid, Stack, Tab, Tabs } from "@mui/material"
 import { useState } from "react"
 
 const GraphExamples: React.FC = () => {
@@ -13,7 +13,12 @@ const GraphExamples: React.FC = () => {
                 <Tab label="Meshgrid - no variance" value={3} />
             </Tabs>
         </Box>
-        <Box component="img" src={`img/benchmark_graph_${tab}.gif`} alt="Animation of graph" />
+        <Stack direction="row" spacing={2}>
+            <Stack direction="column" justifyContent="center">
+                <Box component="img" src={`img/benchmark_example_${tab}.png`} sx={{maxWidth: '350px', maxHeight: '350px', heigth: 'auto'}} alt="Example field" />
+            </Stack>
+            <Box component="img" src={`img/benchmark_graph_${tab}.gif`} alt="Animation of graph" className="fragment" data-fragment-index="1"/>
+        </Stack>
         </>)
 }
 
