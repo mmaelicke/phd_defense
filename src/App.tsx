7850de7e50d1a8089edcf29b5a0ade5a36be72ba
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-09-21 08:50:09 +0200

    use title slide

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { H1, H2, RevealJS, Slide } from "@gregcello/revealjs-react";
+import { RevealJS } from "@gregcello/revealjs-react";
 import { HighlightPlugin, NotesPlugin } from "@gregcello/revealjs-react";
-import { CssBaseline, Grid } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 
+import Title from "./slides/Title";
 import Motivation from "./chapters/Motivation";
 import Introduction from './chapters/Introduction';
 import Uncertainty from './chapters/Uncertainty';
 import SoilMoisture from './chapters/SoilMoisture';
+import Conclusion from "./chapters/Conclusion";
 import Appendix from './chapters/Appendix';
 
-//import 'reveal.js/plugin/highlight/monokai.css'
-//import 'reveal.js/plugin/highlight/zenburn.css'
-//import  'reveal.js/plugin/highlight/highlight'
-import Conclusion from "./chapters/Conclusion";
-
+// import KaTeX css
 import 'katex/dist/katex.min.css'
+
 
 function App() {
 
@@ -29,33 +28,10 @@ function App() {
         hash
         autoAnimateDuration={1.4}
         controlsTutorial={false}
-
       >
-        
         {/* Title slide */}
-        <Slide autoAnimate>
-            <Grid container spacing={0} className="justify-col" sx={{height: '100vh'}}>
-                <Grid item xs={12} className="justify-row">
-                    <img src="img/logo_kit.png" alt="KIT logo" style={{maxHeight: '150px'}} />
-                    <span />
-                </Grid>
-                <Grid item xs={12}>
-                    <H1>
-                        From <span className="fragment highlight-current-red">method development</span> to <span className="fragment highlight-current-red">software integration</span>: A comprehensive approach to <span className="fragment highlight-red">geostatistical</span> uncertainty
-                    </H1>
-                    <H2 style={{marginTop: '8rem'}}>PhD Defense by Mirko Mälicke</H2>
-                </Grid>
-                <Grid item xs={12}></Grid>
-            </Grid>
+        <Title />
 
-            <aside className="notes">
-              <p>Welcome to my PhD defense presentation, titled 'From method development to software integration: A comprehensive approach to geostatistical uncertainty.' For the next 30 minutes, let me take you on a journey</p>
-              <p> about new exciting methods, and...</p>
-              <p>and sophisticated software solutions,</p>
-              <p>all based in the field of geostatistics. Why geostatistics? Is it still relevant in times of Big Data and AI?</p>
-            </aside>
-        </Slide>
-        
         {/* Presentation chapters */}
         <Motivation />
         <Introduction />
